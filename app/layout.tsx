292745cee420cd901adc33e360b5ad834eeb3151
Lordyasith <yasithsamarasinghe12@gmail.com>
2023-08-30 15:23:30 +0530

    second

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,35 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-import { ModalProvider } from '@/components/modal-provider'
+
 import { ToasterProvider } from '@/components/toaster-provider'
+import { ModalProvider } from '@/components/modal-provider'
 import { CrispProvider } from '@/components/crisp-provider'
 
-const inter = Inter({ subsets: ['latin'] })
+import './globals.css'
+
+const font = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'A1',
   description: 'AI Platform',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <CrispProvider/>
-      <body className={inter.className}>
-        <ModalProvider/>
-        <ToasterProvider/>
-        {children}
+      <html lang="en" suppressHydrationWarning>
+        <CrispProvider />
+        <body className={font.className}>
+          <ToasterProvider />
+          <ModalProvider />
+          {children}
         </body>
-    </html>
+      </html>
     </ClerkProvider>
   )
 }
